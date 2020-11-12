7e07dac592bf3a3e5a7b8f92bea6265aa9ef6a31
elxos <eelxos@gmail.com>
2020-11-12 10:59:58 -0400

    Se agrego archivo entry point con datos personales

---
+++ SEjemplo/PEjemplo/principal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre: " << "Alex Rodrigo" << endl;
+	cout << "Apellidos: " << "Duran Moyata" << endl;
+	cout << "CI: " << "10553736Pt" << endl;
+	cout << "CU: " << "35-4702" << endl;
+	system("pause");
+	return 0;
+}
